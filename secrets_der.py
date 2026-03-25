127b3bdaaaceaf75940abcf49ebfb84049b5ec21
Patsy-0 <124100589+Patsy-0@users.noreply.github.com>
2026-03-24 19:13:35 -0600

    Add files via upload

---
+++ secrets_der.py
@@ -0,0 +1,12 @@
+DEVICE_CERT_DER_B64 = """
+Inserta aquí tus certificados
+"""
+
+PRIVATE_KEY_DER_B64 = """
+Inserta aquí tus certificados
+"""
+
+
+ROOT_CA_DER_B64 = """
+Inserta aquí tus certificados
+"""
